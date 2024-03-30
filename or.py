6db815d72b1799e7a9be736f573f79533053661b
Liudmyla <luda.skopenko80@gmail.com>
2024-03-31 01:42:38 +0300

    added recursion

--- or.py
+++ or.py
@@ -2,13 +2,14 @@ import sys
 import pathlib
 
 
-def recursive_print(path):
+def recursive_print(path, depth=0, margin_sumbol='-'):
+    margin = margin_sumbol * depth
     if path.is_dir():
-        print(path.name + '/')
+        print(margin + path.name + '/')
         for item in path.iterdir():
-            recursive_print(item)
+            recursive_print(item, depth=depth +1)
     else:
-        print(path.name)
+        print(margin + path.name)
     
 def main():
    path = pathlib.Path(sys.argv[1])
