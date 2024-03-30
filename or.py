a353e9f12c72204c7e0b805ad5ffee304a724256
Liudmyla <luda.skopenko80@gmail.com>
2024-03-31 01:27:21 +0300

    added folder

--- or.py
+++ or.py
@@ -2,16 +2,17 @@ import sys
 import pathlib
 
 
-def recursive_ptint(path):
-    if path is dir():
-        print(path.name() + '/')
+def recursive_print(path):
+    if path.is_dir():
+        print(path.name + '/')
+        for item in path.iterdir():
+            recursive_print(item)
     else:
         print(path.name)
-    for item in path.iterdir():
-        recursive_ptint(item)
+    
 def main():
    path = pathlib.Path(sys.argv[1])
-   recursive_ptint(path)
+   recursive_print(path)
 
 
 
