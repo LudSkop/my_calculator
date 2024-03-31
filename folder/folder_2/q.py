39e33a3a8c27ffdc27c9c8c0d348b83600ef26fd
Liudmyla <luda.skopenko80@gmail.com>
2024-04-01 02:12:11 +0300

    make q.py

--- folder/folder_2/q.py
+++ folder/folder_2/q.py
@@ -1,4 +1,5 @@
-def  pay_payment(x, y, a, ):
+def  pay_payment(x, y, a = 10 ):
+    print(a)
     result = x + y - a
     return result
 
@@ -6,5 +7,5 @@ def  pay_payment(x, y, a, ):
 
 
 
-res = pay_payment(167, 37, 100, )
+res = pay_payment(167, 37,  )
 print(res)
