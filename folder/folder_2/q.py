b50d6335695d0e161abefc2ca7d89fb515b92418
Liudmyla <luda.skopenko80@gmail.com>
2024-04-01 03:29:56 +0300

    make args

--- folder/folder_2/q.py
+++ folder/folder_2/q.py
@@ -1,11 +1,16 @@
-def  pay_payment(x, y, a = 10 ):
-    print(a)
-    result = x + y - a
-    return result
+def  pay_payment(params_1,  *args , **kwargs):
+    print(params_1)
+    print( args)
+    print(type(args))
+    print(kwargs)
+    print(type(kwargs))
+    
+   
+    
 
 
 
 
 
-res = pay_payment(167, 37,  )
+res = pay_payment(167, 37,53,7, 4, 7, s=234, liuda=200, vlad= 2000, oleg= 1000,   )
 print(res)
