21d80ac102609183e38d8ce88d7d1c110fca813a
Liudmyla <luda.skopenko80@gmail.com>
2024-04-03 01:43:03 +0300

    added sys.argv

--- folder/r.py
+++ folder/r.py
@@ -1,10 +1,15 @@
+import sys
 
-def fib(n):
+
+def fib(n: int)->int:
+    n = int(n)
+    if n == 0:
+        return 0
     if n in(1, 2):
         return 1
     return fib(n - 1) + fib(n - 2)
 
-print(fib(6))
+print(fib(sys.argv[1]))
 
 
 
