9ce0438ebc90b3e7be19b918a01f219d072f9771
Liudmyla <luda.skopenko80@gmail.com>
2024-04-02 23:09:41 +0300

    make func

--- folder/folder_2/q.py
+++ folder/folder_2/q.py
@@ -1,14 +1,11 @@
+def outer():
+    x = 'local x'
 
-x = 50
-
-def test():
-    global x
-    print(f'x is {x}')
-    x = 2
-    print(f'changed lokal x to {x}')
+    def inner():
+        x = 'nonlocal x'
+        print(f'inner function: {x}')
 
 
-
-
-test()
-print(f'x in the end {x}')
+    inner()
+    print(f'outer func: {x}')
+outer()
