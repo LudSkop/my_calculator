ac026a1be8117e5c34bad9da915a2a8ca29f8d95
Liudmyla <luda.skopenko80@gmail.com>
2024-04-06 00:51:36 +0300

    addet if not

--- i.py
+++ i.py
@@ -2,15 +2,17 @@
 #Make func who take string and return dictionary with ASCII. 
 
 from pprint import pprint
+from colorama import Fore
 
 def anabel(string:str)-> dict:
     dic = {}
     for element in string:
-        dic[element]= ord(element)
+        if  not element in dic:
+            dic[element]= ord(element)
     return dic
     
     
-pprint(anabel('hello world, slisa and, pusha and,'))
+pprint(anabel(Fore.RED + 'hello world, slisa and, pusha and,'))
 
 
 
