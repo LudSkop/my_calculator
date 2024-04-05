f41a71f5855a0e4b4e747ee2cb47573e17278f6a
Liudmyla <luda.skopenko80@gmail.com>
2024-04-05 22:38:08 +0300

    added sum

--- o.py
+++ o.py
@@ -1,10 +1,15 @@
 # Порахувати суму всіх чисел чурез рекурсію.
 # 10 -> 10 + 9 + 8 + 7 + 6 + 5 ..... + 1
 sum = 0
-for num in range(1, 11):
-    sum += num
+# for num in range(1, 11):
+#     sum += num
 
-print(sum)
+# print(sum)
 
-
-
+def sum_number(max_number):
+    if max_number <= 0:
+        return 0
+    elif max_number == 1:
+        return 1
+    return max_number + sum_number(max_number - 1)
+print(sum_number(10))
