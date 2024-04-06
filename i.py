e83f7d0ac35d638f36cd7a58f247dc7c7ba2610e
Liudmyla <luda.skopenko80@gmail.com>
2024-04-06 16:31:11 +0300

    added try except, color, annotations

--- i.py
+++ i.py
@@ -11,18 +11,22 @@ def is_prime(n:int)-> bool:
             return False
     return True    
 
-def main():
-    data ={}
+def main()-> dict[str:list]:
+    data = {}
     data['prime'] = []
     data['composite'] = []
     while True:
-        value =int(input(Fore.RED + 'Please enter the number:  ')) 
-        if is_prime(value):
-            print(Fore.GREEN + f'{value} - це просте число')
-            data['prime'].append(value)
-        else:   
-            print(Fore.LIGHTYELLOW_EX + f'{value} -не є простим числом')
-            data['composite'].append(value)
+        try:
+            value =int(input(Fore.RED + 'Please enter the number:  ')) 
+            if is_prime(value):
+                print(Fore.GREEN + f'{value} - це просте число')
+                data['prime'].append(value)
+            else:   
+                print(Fore.LIGHTYELLOW_EX + f'{value} -не є простим числом')
+                data['composite'].append(value)
+        except ValueError:
+            ...
+        
         if value == 6:
             break
     return data    
@@ -31,7 +35,7 @@ def main():
 if __name__ == "__main__":
    
     tab = tabulate(main(), headers='keys', tablefmt='grid')
-    print(tab)
+    print(Fore.LIGHTBLUE_EX + tab)
 
 
 
