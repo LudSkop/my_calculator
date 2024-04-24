6e3a15852ee7df6fa41e77898f080e8f6da67738
Liudmyla <luda.skopenko80@gmail.com>
2024-04-24 21:41:15 +0300

    added path

---
+++ folder/folder_2/rer.py
@@ -0,0 +1,14 @@
+import random
+
+data = [random.randint(0,10) for _ in range(10)]
+# def seed_list(gred):
+#     for _ in range(10):
+#         data.append(random.randint(0, 10))
+
+    
+def buble_sort(data:list[int])->list[int]:
+    leght_list = len(data)-1
+
+
+
+
