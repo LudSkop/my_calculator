0f0b82d337395d68d30ae299464d77abe6a893a4
Liudmyla <luda.skopenko80@gmail.com>
2024-05-03 01:18:40 +0300

    create with open

--- cul.py
+++ cul.py
@@ -8,11 +8,11 @@ card_numbers = ['5553336668889994', '4444444488888888', '3333556677889908', '234
 def is_valid_card(card:str)-> bool:
     return card.isdigit() and len(card) == 16
 
-for card in card_numbers:
-    string = 'Номер картки: {:<8}  платіжна система: {:^16} картка валідна: {:>16}'
-    print(string.format(card, pay_system.get(int(card[0]), 'Unknown'), str(is_valid_card(card))))
+# for card in card_numbers:
+#     string = 'Номер картки: {:<8}  платіжна система: {:^16} картка валідна: {:>16}'
+#     print(string.format(card, pay_system.get(int(card[0]), 'Unknown'), str(is_valid_card(card))))
 
 
 
-
+print(is_valid_card(card_numbers[1]))
 
---
+++ maker.py
@@ -0,0 +1,13 @@
+
+
+
+
+
+
+
+
+
+
+with open("new.txt", mode="r") as file:
+    a = file.read()
+    print(a)
