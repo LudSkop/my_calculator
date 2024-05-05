f0572c42de03b5e09f0b588208ee5d8d6cac3c7e
Liudmyla <luda.skopenko80@gmail.com>
2024-05-05 23:35:02 +0300

    added Path

--- folder/folder_2/e.py
+++ folder/folder_2/e.py
@@ -3,20 +3,31 @@
 
 
 
-def factorial(n):
-    if n < 2:
-        return 1
-    else:
-        return n * factorial(n - 1)
+# def factorial(n):
+#     if n < 2:
+#         return 1
+#     else:
+#         return n * factorial(n - 1)
     
-def number_of_groups(n, k):
-    a = factorial(n)
-    b = factorial(n - 1)
-    c = factorial(k)
-    return int(a // b * c)
+# def number_of_groups(n, k):
+#     a = factorial(n)
+#     b = factorial(n - 1)
+#     c = factorial(k)
+#     return int(a // b * c)
 
 
 
-print(factorial(7))
-print(number_of_groups(10, 4))
+# print(factorial(7))
+# print(number_of_groups(10, 4))
+
+from pathlib import Path
+
+
+current_dir = Path('.')
+
+#file = current_dir / 'folder_2'/ 'folder_3'/'e.py'
+file = current_dir /'folder_2'/'e.py'
+print(file.exists())
+
+
 
