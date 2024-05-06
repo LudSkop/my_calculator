df6d7865d2d8cd73b2ba18ee0ca7d934a68030d5
Liudmyla <luda.skopenko80@gmail.com>
2024-05-06 14:03:56 +0300

    create **/*.py

--- o.py
+++ o.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 
 current_dir = Path('.')
-for el in current_dir.glob('*.txt'):
+for el in current_dir.glob('**/*.txt'):
     print(el)
 
 print(Path.cwd())
+print(current_dir)
 
